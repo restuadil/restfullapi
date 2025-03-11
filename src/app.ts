e07b272adf549fb12a9a698a29c901a3ab226161
restu adil <restuadil09@gmail.com>
2025-03-11 12:28:54 +0700

    fixing some bugs

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
+import { PrismaClient } from "@prisma/client";
 import express, { Application } from "express";
 
 const app: Application = express();
-
+const prisma = new PrismaClient();
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
@@ -9,5 +10,8 @@ app.get("/", (req, res) => {
 app.get("/api", (req, res) => {
   res.json({ message: "API endpoint is working" });
 });
-
+app.get("/api/users", async (req, res) => {
+  const response = await prisma.user.findMany();
+  res.json(response);
+});
 export default app;
