59ccfbba0992d47b84e3fdb053653a83f80df80a
restu adil <restuadil09@gmail.com>
2025-03-16 13:36:17 +0700

    refactor response

--- src/api/category/category.controller.ts
+++ src/api/category/category.controller.ts
@@ -1,15 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { CategoryService } from "./category.service";
+import { sendResponse } from "../../utils/sendResponse";
 export const CategoryController = {
   getAll: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const response = await CategoryService.get(req.query);
-      res.status(200).json({
-        status: true,
-        statusCode: 200,
-        data: response.response,
-        pagination: response.pagination,
-      });
+      sendResponse(res, 200, response, "Success retrived all categories");
     } catch (error) {
       next(error);
     }
@@ -17,11 +13,7 @@ export const CategoryController = {
   getById: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const response = await CategoryService.getById(req.params.id);
-      res.status(200).json({
-        status: true,
-        statusCode: 200,
-        data: response,
-      });
+      sendResponse(res, 200, response, "Success retrieving category");
     } catch (error) {
       next(error);
     }
@@ -29,11 +21,7 @@ export const CategoryController = {
   create: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const response = await CategoryService.create(req.body);
-      res.status(201).json({
-        status: true,
-        statusCode: 201,
-        data: response,
-      });
+      sendResponse(res, 201, response, "Success Created Category");
     } catch (error) {
       next(error);
     }
@@ -41,11 +29,7 @@ export const CategoryController = {
   update: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const response = await CategoryService.update(req.params.id, req.body);
-      res.status(200).json({
-        status: true,
-        statusCode: 200,
-        data: response,
-      });
+      sendResponse(res, 200, response, "Success Update Category");
     } catch (error) {
       next(error);
     }
@@ -53,11 +37,7 @@ export const CategoryController = {
   delete: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const response = await CategoryService.delete(req.params.id);
-      res.status(200).json({
-        status: true,
-        statusCode: 200,
-        data: response,
-      });
+      sendResponse(res, 200, response, "Success Delete Cateogory");
     } catch (error) {
       next(error);
     }
