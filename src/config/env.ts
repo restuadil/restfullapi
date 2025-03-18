53d51d6c8820621f4defc0c01ba897c68fbeff7f
restu adil <restuadil09@gmail.com>
2025-03-18 13:38:36 +0700

    add JWT_SECRET

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,6 +7,10 @@ const envSchema = z.object({
     .enum(["development", "production", "test"])
     .default("development"),
   LOG_LEVEL: z.enum(["error", "warn", "info", "debug"]).default("info"),
+  PRIVATE_KEY: z.string(),
+  PUBLIC_KEY: z.string(),
+  JWT_EXPIRATION_TIME: z.string().transform(Number),
+  BCRYPT_SALT: z.string().default("10").transform(Number),
 });
 
 // Validasi environment variables
